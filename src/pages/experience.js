de23f477867dcebce1530e6b10154c4e2fbb7bf3
Ken O'Hanlon <fj7615@wayne.edu>
2026-01-31 15:41:01 -0500

    #3 KO - Addition of rel tag to embedded word doc

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -4,7 +4,11 @@ const Experience = () => {
   return (
     <div className='resume-container'>
       <div className='grid-item'>
-          <iframe title="Word Resume" src="https://1drv.ms/w/c/c93dfaff3d80cf23/IQTXw37N-IY4QYaFap9Syf4SARPXF2t1ZW3HtDjxQiGBNks?em=2&amp;wdEmbedCode=0" width="100%" height="600px" frameborder="0">This is an embedded <a target="_blank" rel="noreferrer" href="https://office.com">Microsoft Office</a> document, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+          <iframe title="Word Resume" src="https://1drv.ms/w/c/c93dfaff3d80cf23/IQTXw37N-IY4QYaFap9Syf4SARPXF2t1ZW3HtDjxQiGBNks?em=2&amp;wdEmbedCode=0" width="100%" height="600px" frameborder="0">
+              This is an embedded
+              <a target="_blank" rel="noreferrer" href="https://office.com">Microsoft Office</a>
+              document, powered by
+              <a target="_blank" rel="noreferrer" href="https://office.com/webapps">Office</a>.</iframe>
       </div>
     </div>
   )
